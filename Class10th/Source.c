aa62ccbc402d6e1270439eb5493287568a1eedac
leejake9335-cmd <leejake9335@gmail.com>
2026-04-02 14:33:02 +0900

    (2026.03.30) 문자열

---
+++ Class10th/Source.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include<stdarg.h>
+
+
+void execute(int count)
+{
+	if(count <=0)
+	{
+		return;
+	}
+	execute(count - 1);
+
+	printf("count : %d\n",count);
+	
+}
+
+double average(int size, ...)
+{
+	// va_list : 가변 인수의 메모리 주소를 저장하는 포인터 변수입니다.
+	va_list pointer;
+
+	// va_start : 가변 인수를 가져올 수 있도록 포인터를 설정하는 함수입니다.
+	va_start(pointer, size);
+
+	// va_arg
+		va_arg
+		va_end
+}
+int main()
+{
+	
+#pragma region 반환형
+	// 함수가 실행을 마치고, 호출한 쪽으로 어떤 자료형의 값을
+	// 반환할 지 미리 알려주는 형식입니다.
+
+	printf("%d\n", absolute(-7));
+
+	// 함수의 경우 자료형과 반환하는 값의 형태가
+	// 일치하지 않으면 원하는 값을 얻을 수 없습니다.
+#pragma endregion
+
+#pragma region 재귀 함수
+	// 어떤 함수에서 자신을 다시 호출하여 작업을
+	// 수행하는 함수입니다.
+	
+	// execute(3);
+
+	// 재귀 함수는 함수를 계속 호출하기 때문에 스택 영역에
+	// 메모리가 계속 쌓이게 되므로 스택 오버플로우가 발생합니다.
+#pragma endregion
+
+#pragma region 가변 인수 목록
+	// 고정되어 있는 매개 변수 외에 개수가 정해지지 않은
+	// 인수를 추가로 받을 수 있는 인수입니다.
+
+	
+
+
+#pragma endregion
+
+	return 0;
+
+}
--- Class8th (pointer)/Source.c
+++ Class8th (pointer)/Source.c
@@ -45,9 +45,64 @@ void main()
 
 #pragma endregion
 
-#pragma region 숙제
-	// 아스키 코드
-	// 유니 코드  
+#pragma region 문자열
+	// 연속적인 메모리 공간에 저장된 문자 변수의
+	// 집합입니다.
+
+	// const char* message = "invalid";
+	// 
+	// char text[] = "line";
+	// 
+	// printf("message의 값 : %s\n", message);
+	// 
+	// printf("text의 값 : %s\n", text);
+	// 
+	// 문자열의 경우 포인터를 이용하여 문자열 상수를 가리키도록
+	// 할 수 있으며, 문자열 상수는 데이터 영역에 읽기 전용 공간
+	// 에 저장되기 때문에 문자열의 값을 변경할 수 없습니다.
+
+	// message = "error";
+	// 
+	// text[2] = 'm';
+	// 
+	// printf("message의 값 : %s\n", message);
+	// 
+	// printf("text의 값 : %s\n", text);
+
+	// 문자열은 공백도 함께 메모리 공간에 포함하여 크기가 결정되며,
+	// 마지막에 문자열의 끝을 알려주는 제어 문자가 추가 됩니다.
+
+	// text[1] = '\0';
+	// 
+	// printf("text의 값 : %s\n", text);
+	// 
+	// 문자열의 경우 서로 연속적인 메모리 공간으로 연결되어 있지만,
+	// 문자 배열 사이에 무효의 문자를 넣게 되면 무효의 문자 까지만 출력됩니다.
+#pragma endregion
+
+#pragma region (2)차원 배열
+	// 배열의 요소로 또 다른 배열을 가지는 배열입니다.
+
+    //int array2D[3][3] =
+	//{
+	//	{1,2,3},
+	//	{4,5,6},
+	//	{7,8,9},
+	//};
+	//
+	//for (int i = 0; i < 3; i++)
+	//{
+	//	for(int j = 0; j < 3; j++)
+	//	{
+	//		printf("%d",array2D[i][j]);
+	//	}
+	//	printf("\n");
+	//}
+
+
+	// 2차원 배열은 행과 열로 구분되며, 앞에 있는 배열은
+	// 열을 의미하고, 뒤에 있는 배열은 행을 의미합니다.
+
 #pragma endregion
 
 
